c8aa08f15d8b8a28ddd6cd2d4d87907be45824ff
Dorian Shay <dorianshay0806@gmail.com>
2024-01-16 09:08:40 -0700

    Testing done

--- Week_01-15_01-22/Hello.java
+++ Week_01-15_01-22/Hello.java
@@ -1,5 +1,5 @@
 class Hello{
     public static void main(String[] args){
-        System.out.print("My Java program recognizes input: " + args[0]);
+        System.out.print("My Java program recognizes input: " + args[0] + "\n");
     }
 }
